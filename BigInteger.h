1ed2ebf2d50e3d933a266f31273b75370c20a3d2
vandana255 <149075174+vandana255@users.noreply.github.com>
2023-10-26 20:05:16 +0530

    BigInteger.h

--- BigInteger.h
+++ BigInteger.h
@@ -1,3 +1,15 @@
+#ifndef BigInteger_h
+#define BigInteger_h
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct BigInteger {
+    struct Node* head;
+};
+
 struct BigInteger initialize(char* s);
 void display(struct BigInteger a);
 struct BigInteger add(struct BigInteger a, struct BigInteger b);
@@ -5,3 +17,6 @@ struct BigInteger sub(struct BigInteger a, struct BigInteger b);
 struct BigInteger mul(struct BigInteger a, struct BigInteger b);
 struct BigInteger multiplyByDigit(struct BigInteger a, int digit);
 struct Node* addTwoNumbersHelper(struct Node* num1, struct Node* num2, int* carry);
+
+
+#endif
